e46bfb7da804e3c4e39e467decb2f9c38fa8a74b
NIKITA <putnik824@gmail.com>
2022-09-15 17:29:28 +0700

    деление случаный чисел

---
+++ C#/sum/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random ().Next (0,100);
+double numberB = new Random ().Next (0,100);;
+double result = numberA/numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
